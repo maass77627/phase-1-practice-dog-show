2605b19f87eebae480001a60bb5936066cc234b5
Kelly Maass <kmaass77627@gmail.com>
2025-11-26 15:32:20 -0600

    done

--- src/index.js
+++ src/index.js
@@ -2,36 +2,49 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
-fetch('http://localhost:3003/dogs')
+fetch('http://localhost:3000/dogs')
 .then((response) => response.json())
 .then((json) => addDogs(json))
 
 
 function addDogs(dogs) {
+    for (let dog of dogs) {
 
-for (let dog of dogs) {
-    console.log(dog)
-    let tr = document.createElement('tr')
-    let td1 = document.createElement('td')
-    td1.innerHTML = dog.name
-    let td2 = document.createElement('td')
-    td2.innerHTML = dog.breed
-    let td3 = document.createElement('td')
-    td3.innerHTML = dog.sex
-    let td4 = document.createElement('td')
-    let button = document.createElement('button')
-    console.log(dog)
-    button.addEventListener('click', (e) => {populateForm( e.target.parentNode.parentNode.childNodes[0].innerHTML, e.target.parentNode.parentNode.childNodes[1].innerHTML,  e.target.parentNode.parentNode.childNodes[2].innerHTML, dogs)})
-    button.innerHTML = "EDIT DOG"
-    td4.appendChild(button)  
-    tr.appendChild(td1)       
-    tr.appendChild(td2)        
-    tr.appendChild(td3)
-    tr.appendChild(td4)
-    let table = document.getElementById("table-body")
-    table.appendChild(tr)
+        let tr = document.createElement('tr')
+        let td1 = document.createElement('td')
+        td1.innerHTML = dog.name
+        tr.appendChild(td1)
+
+        let td2 = document.createElement('td')
+        td2.innerHTML = dog.breed
+        tr.appendChild(td2)
+        let td3 = document.createElement('td')
+        td3.innerHTML = dog.sex
+        tr.appendChild(td3)
+        let td4 = document.createElement('td')
+        let button = document.createElement('button')
+        button.innerHTML = "Edit Dog"
+        td4.appendChild(button)
+        button.addEventListener('click', (e) => {
+            console.log(e.target.parentNode.parentNode.childNodes[0].innerHTML)
+            populateForm(e.target.parentNode.parentNode.childNodes[0].innerHTML, e.target.parentNode.parentNode.childNodes[1].innerHTML,  e.target.parentNode.parentNode.childNodes[2].innerHTML, dogs)
+
+        })
+        
+
+        tr.appendChild(td4)
+
+        let table = document.getElementById("table-body")
+        table.appendChild(tr)
+    }
 }
-}
+
+})
+
+
+
+
+
 
 
 
@@ -49,18 +62,18 @@ console.log(dog)
     nextfunc(dog)
     }
 
-    //function formform(){
+   
     function nextfunc(dog) {
     let form = document.getElementById('dog-form')
     form.addEventListener('submit', (e) => {
     e.preventDefault()
-    //console.log(e.target)
+    
     let name = e.target.name.value
     let breed = e.target.breed.value
     let sex = e.target.sex.value
     console.log(dog.id)
     
-    fetch(`http://localhost:3003/dogs/${dog.id}`, {
+    fetch(`http://localhost:3000/dogs/${dog.id}`, {
         method: "PATCH",
         headers: {
             'Content-type': 'application/json'
@@ -71,4 +84,3 @@ console.log(dog)
     .then((json) => console.log(json))
 })
         }
-})
